07fe58b2e3b240e7ad283ecffb3a259c4e680bfc
VovaKyrylenko <VovaKyrylenko@users.noreply.github.com>
2024-03-21 19:35:44 +0000

    deploy: 84fc106d45f0a15123182cd3941e17f5c4ff9c0b

--- src/components/index.ts
+++
@@ -1,4 +0,0 @@
-export * from "./modals";
-export { Board } from "./board";
-export { BoardCardInfo } from "./board-card-info";
-export { SearchBar } from "./search-bar";
--- src/constants/index.ts
+++
@@ -1,1 +0,0 @@
-export const API_URL = "https://test-task-backend-0w1x.onrender.com";
