4845e85f200800ccaba4e275eca17e086ae6371b
Volodymyr Kyrylenko <vovichkyry@gmail.com>
2024-03-21 20:01:54 +0200

    Added config of main files

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <>
     <App />
-  </React.StrictMode>,
-)
+    <ToastContainer />
+  </>
+);
